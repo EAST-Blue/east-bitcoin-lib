48f3200db5a42c6dfae95f162ac71bf072b8663a
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-08-05 13:40:40 +0700

    update: sdk opreturn support raw scripts

--- packages/sdk/src/addresses/opReturn.ts
+++ packages/sdk/src/addresses/opReturn.ts
@@ -4,15 +4,26 @@ import { Script, StackScripts } from "../script";
 import { OpReturnType } from "./types";
 
 export type OpReturnParams = {
-  dataScripts: StackScripts;
+  dataScripts?: StackScripts;
+  buffer?: Buffer;
 };
 
 export class OpReturn {
   type: OpReturnType = "op_return";
   script: Buffer;
 
-  constructor({ dataScripts }: OpReturnParams) {
-    const finalScripts = [Script.OP_RETURN, ...dataScripts];
-    this.script = Script.compile(finalScripts);
+  constructor({ dataScripts, buffer }: OpReturnParams) {
+    if (!(dataScripts || buffer)) {
+      throw new Error("errors.provide either dataScripts or buffer");
+    }
+
+    if (dataScripts) {
+      const finalScripts = [Script.OP_RETURN, ...dataScripts];
+      this.script = Script.compile(finalScripts);
+
+      return;
+    }
+
+    this.script = buffer!;
   }
 }
