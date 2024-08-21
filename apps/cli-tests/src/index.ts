a9bdbe4e47c47147c18b4e65657df2a98f82c8c7
Irfi M <irfi@localcitizen>
2024-08-21 20:57:38 +0700

    script: all cursed

--- apps/cli-tests/src/index.ts
+++ apps/cli-tests/src/index.ts
@@ -13,7 +13,6 @@ import {
 } from "@east-bitcoin-lib/sdk";
 import { Input } from "@east-bitcoin-lib/sdk/dist/psbt/types";
 import { BitcoinUTXO } from "@east-bitcoin-lib/sdk/dist/repositories/bitcoin/types";
-import { bitcoin } from "bitcoinjs-lib/src/networks";
 
 const walletApi = new BElectrsAPI({
   network: "regtest",
@@ -89,7 +88,6 @@ async function mintInscription(script: (internalPubkey: Buffer) => P2trScript) {
   await regboxApi.generateBlock(1);
   await sleep(5000);
 
-
   // inscription commit address
   console.log({ address: p2tr.address });
 
@@ -104,7 +102,7 @@ async function mintInscription(script: (internalPubkey: Buffer) => P2trScript) {
       },
     ],
     feeRate: 1,
-    changeOutput: Address.fromString(p2tr.address),
+    changeOutput: Address.fromString(wallet.p2pkh(0).address),
     autoUtxo: {
       api,
       from: new P2trAutoUtxo(p2tr),
@@ -129,7 +127,8 @@ async function mintInscriptionText() {
       Script.OP_FALSE,
       Script.OP_IF,
       Script.encodeUTF8("ord"),
-      Script.OP_1,
+      1,
+      1, // OP_PUSHBYTES_1 1
       Script.encodeUTF8("text/plain;charset=utf-8"),
       Script.OP_0,
       Script.encodeUTF8("Nasi Padang Guguak"),
@@ -169,7 +168,8 @@ async function mintInscriptionImage() {
       Script.OP_FALSE,
       Script.OP_IF,
       Script.encodeUTF8("ord"),
-      Script.OP_1,
+      1,
+      1, // OP_PUSHBYTES_1 1
       Script.encodeUTF8("image/png"),
       Script.OP_0,
       imageBuffer,
@@ -209,7 +209,7 @@ async function mintInscriptionCursedIncompleteField() {
       Script.OP_FALSE,
       Script.OP_IF,
       Script.encodeUTF8("ord"),
-      Script.OP_1,
+      1, // OP_PUSHBYTES_1
       Script.OP_0,
       imageBuffer,
       Script.OP_ENDIF,
@@ -252,9 +252,9 @@ async function mintInscriptionCursedNotAtOffset0() {
       Script.OP_FALSE,
       Script.OP_IF,
       Script.encodeUTF8("ord"),
-      Script.OP_1,
+      1, 1,
       Script.encodeUTF8("image/png"),
-      Script.OP_2,
+      1, 2,
       Buffer.from([251]),
       Script.OP_0,
       imageBuffer,
@@ -294,9 +294,54 @@ async function mintInscriptionCursedPushnum() {
       Script.OP_FALSE,
       Script.OP_IF,
       Script.encodeUTF8("ord"),
+      Script.OP_1, // this is pushnum
+      Script.encodeUTF8("image/png"),
       Script.OP_0,
       imageBuffer,
-      Script.OP_13,
+      Script.OP_ENDIF,
+    ]);
+    const recovery = Script.compile([internalPubkey, Script.OP_CHECKSIG]);
+
+    return {
+      taptree: [
+        {
+          output: Script.compile(inscription),
+        },
+        {
+          output: Script.compile(recovery),
+        },
+      ],
+      redeem: {
+        output: inscription,
+        redeemVersion: 192,
+      },
+    };
+  };
+
+  return mintInscription(script);
+}
+
+async function mintInscriptionCursedStutter() {
+  const imageBase64 =
+    "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKBAMAAAB/HNKOAAAAD1BMVEVbHx6uTEHvrJjbjHrLbFynATLxAAAAIUlEQVQI12NgwASMjgJAUljJEMh0VnIEslmUQCKMxqjqADLDAcrdvFBmAAAAAElFTkSuQmCC";
+
+  const imageBuffer = Buffer.from(imageBase64, "base64");
+
+  const script = (internalPubkey: Buffer): P2trScript => {
+    const inscription = Script.compile([
+      internalPubkey,
+      Script.OP_CHECKSIG,
+      Script.OP_FALSE,
+      Script.OP_IF,
+      Script.OP_FALSE,
+      Script.OP_IF,
+      Script.encodeUTF8("ord"),
+      1,
+      1,
+      Script.encodeUTF8("image/png"),
+      Script.OP_0,
+      imageBuffer,
+      Script.OP_ENDIF,
       Script.OP_ENDIF,
     ]);
     const recovery = Script.compile([internalPubkey, Script.OP_CHECKSIG]);
@@ -333,9 +378,11 @@ async function mintInscriptionCursedDuplicateFields() {
       Script.OP_FALSE,
       Script.OP_IF,
       Script.encodeUTF8("ord"),
-      Script.OP_1,
+      1,
+      1,
       Script.encodeUTF8("image/png"),
-      Script.OP_1,
+      1,
+      1,
       Script.encodeUTF8("image/jpeg"),
       Script.OP_0,
       imageBuffer,
@@ -369,7 +416,9 @@ async function mintAndTransferInscription() {
   const newOwner = wallet.p2wpkh(1);
   console.log("new owner", newOwner.address);
 
-  const inputs = await getInputsByOutpoints(oldOwner.address, [{ hash: inscriptionTxHash, index: 0 }]);
+  const inputs = await getInputsByOutpoints(oldOwner.address, [
+    { hash: inscriptionTxHash, index: 0 },
+  ]);
 
   const p = new PSBT({
     network: "regtest",
@@ -398,6 +447,113 @@ async function mintAndTransferInscription() {
   return txHash;
 }
 
+async function mintInscriptionCursedReinscription() {
+  // 1. MINT
+  const inscriptionTxHash = await mintInscriptionImage();
+  await sleep(5000);
+
+  console.log("inscriptionTxHash", inscriptionTxHash);
+
+  const oldOwner = wallet.p2wpkh(0);
+  await regboxApi.getFaucet(oldOwner.address, 1);
+  await sleep(5000);
+
+  const inputs = await getInputsByOutpoints(oldOwner.address, [
+    { hash: inscriptionTxHash, index: 0 },
+  ]);
+  // 2. TRANSFER TO newOwner
+
+  // script for step 3 (reinscription)
+  const script = (internalPubkey: Buffer): P2trScript => {
+    const inscription = Script.compile([
+      internalPubkey,
+      Script.OP_CHECKSIG,
+      Script.OP_FALSE,
+      Script.OP_IF,
+      Script.encodeUTF8("ord"),
+      1,
+      1, // OP_PUSHBYTES_1 1
+      Script.encodeUTF8("text/plain;charset=utf-8"),
+      Script.OP_0,
+      Script.encodeUTF8("Nasi Padang Guguak"),
+      Script.OP_ENDIF,
+    ]);
+    const recovery = Script.compile([internalPubkey, Script.OP_CHECKSIG]);
+
+    return {
+      taptree: [
+        {
+          output: Script.compile(inscription),
+        },
+        {
+          output: Script.compile(recovery),
+        },
+      ],
+      redeem: {
+        output: inscription,
+        redeemVersion: 192,
+      },
+    };
+  };
+
+  const p2trReinscription = wallet.p2trScript(0, script);
+  await regboxApi.getFaucet(p2trReinscription.address, 1);
+  await sleep(5000);
+
+  const p = new PSBT({
+    network: "regtest",
+    inputs: inputs,
+    outputs: [
+      {
+        // this will become the owner of the insription
+        output: Address.fromString(p2trReinscription.address),
+        value: 600,
+      },
+    ],
+    feeRate: 1,
+    changeOutput: Address.fromString(oldOwner.address),
+    autoUtxo: {
+      api,
+      from: new P2wpkhAutoUtxo(oldOwner),
+    },
+  });
+
+  await p.build();
+  p.signAllInputs(oldOwner.keypair);
+  p.finalizeAllInputs();
+
+  const txHash = await walletApi.brodcastTx(p.toHex(true));
+  await sleep(5000);
+
+  // 3. MINT AGAIN
+
+  const reinscriptionP = new PSBT({
+    network: "regtest",
+    inputs: [],
+    outputs: [
+      {
+        // this will become the owner of the insription
+        output: Address.fromString(wallet.p2wpkh(0).address),
+        value: 600,
+      },
+    ],
+    feeRate: 1,
+    changeOutput: Address.fromString(wallet.p2pkh(0).address),
+    autoUtxo: {
+      api,
+      from: new P2trAutoUtxo(p2trReinscription),
+    },
+  });
+
+  await reinscriptionP.build();
+  reinscriptionP.signAllInputs(p2trReinscription.keypair);
+  reinscriptionP.finalizeAllInputs();
+
+  const remintTxHash = await walletApi.brodcastTx(reinscriptionP.toHex(true));
+  console.log({ hex: p.toHex(true), txHash: remintTxHash });
+  return txHash;
+}
+
 async function main() {
   const p2wpkhAddress = wallet.p2wpkh(0).address;
   console.log("p2wpkh address: ", p2wpkhAddress);
@@ -414,7 +570,7 @@ async function main() {
   // mintInscriptionCursedDuplicateFields()
 
   // IncompleteField - key without value
- // mintInscriptionCursedIncompleteField()
+  // mintInscriptionCursedIncompleteField()
   // NotAtOffsetZero - ins.offset != 0
   // mintInscriptionCursedNotAtOffset0()
   // NotInFirstInput - ins.input_index != 0
@@ -424,9 +580,12 @@ async function main() {
   // mintInscriptionCursedNotAtOffset0()
 
   // Pushnum - using OP_PUSHNUM before jubilee will be cursed (jubilee 824544)
-  mintInscriptionCursedPushnum()
+  // mintInscriptionCursedPushnum();
   // Reinscription - using reinscription is cursed
+  // mintInscriptionCursedReinscription()
   // Stutter - for inscriptions which start with OP_FALSE OP_FALSE OP_IF or OP_FALSE OP_IF OP_FALSE OP_IF https://github.com/ordinals/ord/issues/2693         // UnrecognizedEvenField - check even field (i % 2 == 0), if not recognize then cursed
+  mintInscriptionCursedStutter()
+
 
   // PROVENANCE
 
